bd3bb854121758fe903a056722be754241515621
Gautham Sajith <gsajith@umich.edu>
2021-05-28 16:32:40 -0400

    Delayed gyro

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 import Head from "next/head";
 import FullScreen from "../components/FullScreen";
 import Tilt from "react-parallax-tilt";
+import { useState } from "react";
 
 export default function Home() {
+  const [showGyro, setShowGyro] = useState(true);
+
   return (
     <FullScreen>
       <Head>
@@ -10,12 +13,21 @@ export default function Home() {
         <meta name="description" content="Parallax Maker" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Tilt gyroscope={true} tiltMaxAngleX={45} tiltMaxAngleY={45}>
-        <img
-          src="https://i.imgur.com/92G2k5D.jpg"
-          style={{ width: "90vw", maxWidth: 500 }}
-        />
-      </Tilt>
+      <button
+        onClick={() => {
+          setShowGyro(false);
+          setTimeout(() => setShowGyro(true), 1000);
+        }}>
+        Show gyro
+      </button>
+      {showGyro && (
+        <Tilt gyroscope={true} tiltMaxAngleX={45} tiltMaxAngleY={45}>
+          <img
+            src="https://i.imgur.com/92G2k5D.jpg"
+            style={{ width: "90vw", maxWidth: 500 }}
+          />
+        </Tilt>
+      )}
     </FullScreen>
   );
 }

32fc7f39faa3d49527bc5309ecb83b448a733139
Gautham Sajith <gsajith@umich.edu>
2021-05-28 16:34:05 -0400

    Set VH on mobile

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,21 @@
 import Head from "next/head";
 import FullScreen from "../components/FullScreen";
 import Tilt from "react-parallax-tilt";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { isMobile } from "react-device-detect";
 
 export default function Home() {
   const [showGyro, setShowGyro] = useState(true);
+
+  useEffect(() => {
+    if (isMobile) {
+      // https://css-tricks.com/the-trick-to-viewport-units-on-mobile/
+      // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+      let vh = window.innerHeight * 0.01;
+      // Then we set the value in the --vh custom property to the root of the document
+      document.documentElement.style.setProperty("--vh", `${vh}px`);
+    }
+  }, [isMobile]);
 
   return (
     <FullScreen>
